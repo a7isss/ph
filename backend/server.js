a0ed9c0ddf3407afca578ea04dfaa4585ce1f30b
A7isss <10.younis@gmail.com>
2025-03-19 01:13:35 +0300

    Updated links to fe and admin

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,14 @@ connectCloudinary()
 
 // middlewares
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: [
+    'ph-hpgz-ckschcoys-a7isss-projects.vercel.app', // Replace with your actual frontend URL
+    'ph-lr89-p77qd1unt-a7isss-projects.vercel.app'    // Replace with your actual admin URL
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+  credentials: true, // Allow cookies and authorization headers
+}));
 
 // api endpoints
 app.use("/api/user", userRouter)
