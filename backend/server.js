14065e7a3c5a0d27bd53646ba9c2dc0e5e81defd
A7isss <10.younis@gmail.com>
2025-03-19 05:17:06 +0300

    Your commit message here

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ import adminRouter from "./routes/adminRoute.js"
 // app config
 const app = express()
 const port = process.env.PORT || 4000
+app.listen(port, () => console.log(`Server started on PORT:${port}`));
 connectDB()
 connectCloudinary()
 
