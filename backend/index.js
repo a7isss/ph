d23265223b69818ba9aeb081c7241fafd7580c58
A7isss <10.younis@gmail.com>
2025-03-19 06:19:03 +0300

    Your commit message here

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,15 @@
 // index.js
-const express = require('express')
-const app = express()
-const PORT = 4000
-
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 4000; // Use dynamic port for Vercel
 
 app.get('/home', (req, res) => {
   res.status(200).json('Welcome, your app is working well');
-})
-
+});
 
 app.listen(PORT, () => {
-  console.log(`Server running at https://ph-red.vercel.app:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
 // Export the Express API
-module.exports = app
+module.exports = app;
