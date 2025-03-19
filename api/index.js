4774c4c86fe21609eea52fecbc894f63d269f7a9
A7isss <10.younis@gmail.com>
2025-03-19 23:53:02 +0300

    Your commit message here

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import { createServer } from "@vercel/node"; // Vercel's serverless adapter
 
 // Dynamically import the ESM module
-const appPromise = import("../backend/server.js").then((module) => module.default);
+const appPromise = import("./server.js").then((module) => module.default);
 
 export default async function handler(req, res) {
   const app = await appPromise; // Wait for the Express app to load
