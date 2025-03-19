95e4558ac66261e3d94853fdffc4435a4a355c0e
A7isss <10.younis@gmail.com>
2025-03-19 22:40:39 +0300

    Your commit message here

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
 import app from "../backend/server"; // Import your Express app
 import { createServer } from "@vercel/node"; // Vercel's serverless adapter
 
-module.exports = createServer(app); // Export the app as a serverless function
+export default createServer(app); // Export the app as a serverless function
