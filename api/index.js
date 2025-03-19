3cb335f7134bd8747f8ac68e17719ffcb830e39f
A7isss <10.younis@gmail.com>
2025-03-19 22:50:32 +0300

    Your commit message here

--- api/index.js
+++ api/index.js
@@ -1,4 +1,9 @@
-import app from "../backend/server"; // Import your Express app
 import { createServer } from "@vercel/node"; // Vercel's serverless adapter
 
-export default createServer(app); // Export the app as a serverless function
+const appPromise = import("../backend/server.js").then((module) => module.default);
+
+export default async function handler(req, res) {
+  const app = await appPromise;
+  const server = createServer(app);
+  return server(req, res);
+}
